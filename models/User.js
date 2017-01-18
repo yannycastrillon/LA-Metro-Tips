c3384701190826a6f9914e6fe50cbe435c4c4ef3
Yanny Castrillon <yannyandres117@gmail.com>
2017-01-18 12:04:57 -0800

    installed bcrypt depency. Created the user model schema with fields firstName, lastName, email and password. Validations of uniqueness and requirements

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const
+  mongoose = require("mongoose"),
+  bycrypt = require("bycrypt-nodejs"),
+  Schema = mongoose.Schema,
+
+  // Build up the schema of the model
+  userSchema = new Schema({
+    firstName:{type:String, require:true},
+    lastName:{type:String,require:true},
+    email:{type:String, require:true, index:{unique:true}},
+    // password will be encrypted as password_digest on DB with bycrypt-nodejs
+    password:{type:String,require:true}
+  })
+
+
+// Creates a hash with a SALT password encrypted
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password,bycrypt.genSaltSync(10))
+}
+
+// Validates the password with the one encrypted on DB.
+userSchema.methods.isValidPassword = function(password) {
+  return bcrypt.compareSync(password,this.password)
+}
+
+
+// exports model so it can be used on other parts of the app
+module.exports = mongoose.model("User",userSchema)
